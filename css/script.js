1422ac5c0daf7e75144a7961aed6c86fa98433fc
sandhiya <sandhiyar211@gmail.com>
2025-12-23 19:40:51 +0530

    Delete css directory

--- css/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Thank you for contacting me! I will get back to you soon.");
-});
